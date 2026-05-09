6d025f3c2d45cdb2cb7ca99f4c744cfaffdb99f6
zysenia <marvin.ms.wr@outlook.de>
2026-05-09 03:07:38 +0200

    feature: add additional quote fields and update fixtures

--- tests/test_stock.py
+++ tests/test_stock.py
@@ -168,10 +168,14 @@ def mocked_responses():
                     {
                         "rics": "TSLA.GTX",
                         "fids": (
-                            "x._TICKER,x._DSPLY_NAME,q._TRDPRC_1,q._NETCHNG_1,"
-                            "q._PCTCHNG,q._COUNTRY,q._TRADE_DATE,q._TRDTIM_1,"
-                            "x._LOCAL_ID,q._OPEN_PRC,q._HIGH_1,q._LOW_1,"
-                            "q._TURNOVER,rkd.COMP_TAXONOMY_TRBC_CD_L1"
+                            "x._TICKER,x._DSPLY_NAME,x._LOCAL_ID,x._ISIN,x._CURRENCY,"
+                            "q._TRDPRC_1,q._NETCHNG_1,q._PCTCHNG,q._BID,q._ASK,"
+                            "q._BIDSIZE,q._ASKSIZE,q._ACVOL_1,q._TURNOVER,q._VWAP,"
+                            "q._TRDVOL_1,q._OPEN_PRC,q._HIGH_1,q._LOW_1,q._HST_CLOSE,"
+                            "q._COUNTRY,q._TRADE_DATE,q._TRDTIM_1,q._HSTCLSDATE,"
+                            "rkd.COMP_TAXONOMY_TRBC_CD_L1,"
+                            "rkd.COMP_TAXONOMY_TRBC_CD_L2,"
+                            "rkd.COMP_TAXONOMY_TRBC_CD_L3"
                         ),
                     }
                 )
@@ -188,31 +192,47 @@ def test_stock(mocked_responses):
     s = Stock("US88160R1014")
     assert s.isin == "US88160R1014"
 
-    assert s.bid_price == 290.1
-    assert s.ask_price == 290.25
+    assert s.bid_price == 363.05
+    assert s.ask_price == 363.45
 
     assert s.bid_size == 375
-    assert s.ask_size == 365
+    assert s.ask_size == 375
 
     sleep(2.5)
 
     assert s.ticker == "TL0"
     assert s.display_name == "TESLA MOTORS"
-    assert s.low_price == 274.4
-    assert s.price_change == 14.55
-    assert s.percent_change == 5.283
+    assert s.low_price == 350.55
+    assert s.price_change == 13.1
+    assert s.percent_change == 3.741
     assert s.country == "DE"
     assert s.trade_date_time == datetime(
-        year=2025,
-        month=8,
-        day=22,
+        year=2026,
+        month=5,
+        day=8,
         hour=20,
-        minute=57,
+        minute=56,
+        second=10,
         tzinfo=timezone.utc,
     )
     assert s.wkn == "A1CX3T"
-    assert s.open_price == 275.75
-    assert s.high_price == 290.4
-    assert s.last_price == 289.95
-    assert s.turnover == 9747130.85
+    assert s.open_price == 351.05
+    assert s.high_price == 366
+    assert s.last_price == 363.3
+    assert s.turnover == 5112392.25
     assert s.taxonomy == "Consumer"
+    assert s.taxonomy_level_2 == 5310
+    assert s.taxonomy_level_3 == 531010
+    assert s.currency == "EUR"
+    assert s.accumulated_intraday_volume == 14201
+    assert s.vwap == 360.00227
+    assert s.last_trade_volume == 1
+    assert s.previous_close_price == 350.2
+    assert s.previous_close_date == datetime(
+        year=2026,
+        month=5,
+        day=7,
+        tzinfo=timezone.utc,
+    )
+
+    assert False, s._get_quote_info_instrument_info()
